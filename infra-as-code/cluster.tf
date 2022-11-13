c42a28b451b4cee6d482af6dbda558433404ffce
EC2 Default User <ec2-user@ip-172-31-61-28.ec2.internal>
2022-11-13 07:32:50 +0000

    version

--- infra-as-code/cluster.tf
+++ infra-as-code/cluster.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "hello_world" {
   name     = var.cluster-name
   role_arn = aws_iam_role.k8s_cluster.arn
+  version = "1.20"
 
   vpc_config {
     security_group_ids = [aws_security_group.k8s_cluster.id]
